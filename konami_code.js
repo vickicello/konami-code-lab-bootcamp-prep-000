38c0778e0485e59c6ada2b750a454382e5952ebc
Vicki Hamm <vicki.cello@gmail.com>
2018-04-24 01:30:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-  
+ 
   let index = 0;
   
   function onKeyDownHandler(e) {
