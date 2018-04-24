c176a4f7f733a5584c04b367c71196b36b697c46
Vicki Hamm <vicki.cello@gmail.com>
2018-04-24 01:34:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,7 +11,7 @@ function init() {
       index+=1;
       
       if (index === code.length) {
-        window.alert("Hurray!");
+        alert("Hurray!");
         index = 0;
       }
     } else {
