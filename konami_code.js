63c6c235948469db90181501d70407f0a029aac5
Vicki Hamm <vicki.cello@gmail.com>
2018-04-24 01:27:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
     
     document.addEventListener('keydown', function(e) {
     const key = 
-    parseInt(e.detail || e.which);
+    parseInt(e.location || e.detail);
     
     if (key === code[index]) {
       index++;
