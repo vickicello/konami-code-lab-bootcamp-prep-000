dccbdd1a12eaf0631cb73e4e7ad9e72693d30d5d
Vicki Hamm <vicki.cello@gmail.com>
2018-04-24 01:27:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
   
   function onKeyDownHandler(e) {
     
-    document.addEventListener('body')
+    document.addEventListener('keydown', )
     const key = 
     parseInt(e.detail || e.which);
     
